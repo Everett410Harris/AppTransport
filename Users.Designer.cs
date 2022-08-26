232439b551085f3e111d5c361a04592057feb034
Everett Harris <everettharr979@gmail.com>
2022-08-26 16:10:37 -0400

    created  and designed users form

---
+++ Users.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace AppTransport
+{
+    partial class Users
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Users));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.userPhoneLabel = new System.Windows.Forms.Label();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.genderCb = new System.Windows.Forms.ComboBox();
+            this.joinDateLabel = new System.Windows.Forms.Label();
+            this.userPhoneTb = new System.Windows.Forms.TextBox();
+            this.userDOBLabel = new System.Windows.Forms.Label();
+            this.passwordTb = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.userNameTb = new System.Windows.Forms.TextBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.usersLabel = new System.Windows.Forms.Label();
+            this.userDOBdateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.joinDatedateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userPicBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitLabel = new System.Windows.Forms.Label();
+            this.exitPicBox = new System.Windows.Forms.PictureBox();
+            this.dashboardLabel = new System.Windows.Forms.Label();
+            this.dashboardPicBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel1.Controls.Add(this.joinDatedateTimePicker1);
+            this.panel1.Controls.Add(this.userDOBdateTimePicker1);
+            this.panel1.Controls.Add(this.userPhoneLabel);
+            this.panel1.Controls.Add(this.genderLabel);
+            this.panel1.Controls.Add(this.genderCb);
+            this.panel1.Controls.Add(this.joinDateLabel);
+            this.panel1.Controls.Add(this.userPhoneTb);
+            this.panel1.Controls.Add(this.userDOBLabel);
+            this.panel1.Controls.Add(this.passwordTb);
+            this.panel1.Controls.Add(this.passwordLabel);
+            this.panel1.Controls.Add(this.userNameTb);
+            this.panel1.Controls.Add(this.userNameLabel);
+            this.panel1.Controls.Add(this.usersLabel);
+            this.panel1.Location = new System.Drawing.Point(213, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1056, 233);
+            this.panel1.TabIndex = 1;
+            // 
+            // userPhoneLabel
+            // 
+            this.userPhoneLabel.AutoSize = true;
+            this.userPhoneLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userPhoneLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.userPhoneLabel.Location = new System.Drawing.Point(108, 189);
+            this.userPhoneLabel.Name = "userPhoneLabel";
+            this.userPhoneLabel.Size = new System.Drawing.Size(125, 30);
+            this.userPhoneLabel.TabIndex = 18;
+            this.userPhoneLabel.Text = "User Phone";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.genderLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.genderLabel.Location = new System.Drawing.Point(608, 135);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(84, 30);
+            this.genderLabel.TabIndex = 17;
+            this.genderLabel.Text = "Gender";
+            // 
+            // genderCb
+            // 
+            this.genderCb.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.genderCb.FormattingEnabled = true;
+            this.genderCb.Items.AddRange(new object[] {
+            "",
+            "Female",
+            "Male",
+            "Other"});
+            this.genderCb.Location = new System.Drawing.Point(744, 127);
+            this.genderCb.Name = "genderCb";
+            this.genderCb.Size = new System.Drawing.Size(225, 38);
+            this.genderCb.TabIndex = 10;
+            // 
+            // joinDateLabel
+            // 
+            this.joinDateLabel.AutoSize = true;
+            this.joinDateLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.joinDateLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.joinDateLabel.Location = new System.Drawing.Point(609, 188);
+            this.joinDateLabel.Name = "joinDateLabel";
+            this.joinDateLabel.Size = new System.Drawing.Size(105, 30);
+            this.joinDateLabel.TabIndex = 7;
+            this.joinDateLabel.Text = "Join Date";
+            // 
+            // userPhoneTb
+            // 
+            this.userPhoneTb.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userPhoneTb.Location = new System.Drawing.Point(263, 184);
+            this.userPhoneTb.Name = "userPhoneTb";
+            this.userPhoneTb.Size = new System.Drawing.Size(225, 35);
+            this.userPhoneTb.TabIndex = 6;
+            // 
+            // userDOBLabel
+            // 
+            this.userDOBLabel.AutoSize = true;
+            this.userDOBLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userDOBLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.userDOBLabel.Location = new System.Drawing.Point(609, 75);
+            this.userDOBLabel.Name = "userDOBLabel";
+            this.userDOBLabel.Size = new System.Drawing.Size(106, 30);
+            this.userDOBLabel.TabIndex = 5;
+            this.userDOBLabel.Text = "User DOB";
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.passwordTb.Location = new System.Drawing.Point(263, 130);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(225, 35);
+            this.passwordTb.TabIndex = 4;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.passwordLabel.Location = new System.Drawing.Point(108, 135);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(105, 30);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            // 
+            // userNameTb
+            // 
+            this.userNameTb.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userNameTb.Location = new System.Drawing.Point(263, 74);
+            this.userNameTb.Name = "userNameTb";
+            this.userNameTb.Size = new System.Drawing.Size(225, 35);
+            this.userNameTb.TabIndex = 2;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userNameLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.userNameLabel.Location = new System.Drawing.Point(108, 79);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(129, 30);
+            this.userNameLabel.TabIndex = 1;
+            this.userNameLabel.Text = "Users Name";
+            // 
+            // usersLabel
+            // 
+            this.usersLabel.AutoSize = true;
+            this.usersLabel.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.usersLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.usersLabel.Location = new System.Drawing.Point(489, 12);
+            this.usersLabel.Name = "usersLabel";
+            this.usersLabel.Size = new System.Drawing.Size(92, 40);
+            this.usersLabel.TabIndex = 0;
+            this.usersLabel.Text = "Users";
+            // 
+            // userDOBdateTimePicker1
+            // 
+            this.userDOBdateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.userDOBdateTimePicker1.Location = new System.Drawing.Point(744, 79);
+            this.userDOBdateTimePicker1.Name = "userDOBdateTimePicker1";
+            this.userDOBdateTimePicker1.Size = new System.Drawing.Size(225, 23);
+            this.userDOBdateTimePicker1.TabIndex = 19;
+            // 
+            // joinDatedateTimePicker1
+            // 
+            this.joinDatedateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.joinDatedateTimePicker1.Location = new System.Drawing.Point(744, 196);
+            this.joinDatedateTimePicker1.Name = "joinDatedateTimePicker1";
+            this.joinDatedateTimePicker1.Size = new System.Drawing.Size(225, 23);
+            this.joinDatedateTimePicker1.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 30);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Users";
+            // 
+            // userPicBox
+            // 
+            this.userPicBox.Image = ((System.Drawing.Image)(resources.GetObject("userPicBox.Image")));
+            this.userPicBox.Location = new System.Drawing.Point(65, 108);
+            this.userPicBox.Name = "userPicBox";
+            this.userPicBox.Size = new System.Drawing.Size(61, 62);
+            this.userPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.userPicBox.TabIndex = 13;
+            this.userPicBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitLabel
+            // 
+            this.exitLabel.AutoSize = true;
+            this.exitLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.exitLabel.Location = new System.Drawing.Point(95, 806);
+            this.exitLabel.Name = "exitLabel";
+            this.exitLabel.Size = new System.Drawing.Size(50, 30);
+            this.exitLabel.TabIndex = 24;
+            this.exitLabel.Text = "Exit";
+            // 
+            // exitPicBox
+            // 
+            this.exitPicBox.Image = ((System.Drawing.Image)(resources.GetObject("exitPicBox.Image")));
+            this.exitPicBox.Location = new System.Drawing.Point(84, 741);
+            this.exitPicBox.Name = "exitPicBox";
+            this.exitPicBox.Size = new System.Drawing.Size(61, 62);
+            this.exitPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.exitPicBox.TabIndex = 23;
+            this.exitPicBox.TabStop = false;
+            // 
+            // dashboardLabel
+            // 
+            this.dashboardLabel.AutoSize = true;
+            this.dashboardLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dashboardLabel.Location = new System.Drawing.Point(47, 305);
+            this.dashboardLabel.Name = "dashboardLabel";
+            this.dashboardLabel.Size = new System.Drawing.Size(119, 30);
+            this.dashboardLabel.TabIndex = 22;
+            this.dashboardLabel.Text = "Dashboard";
+            // 
+            // dashboardPicBox
+            // 
+            this.dashboardPicBox.Image = ((System.Drawing.Image)(resources.GetObject("dashboardPicBox.Image")));
+            this.dashboardPicBox.Location = new System.Drawing.Point(65, 240);
+            this.dashboardPicBox.Name = "dashboardPicBox";
+            this.dashboardPicBox.Size = new System.Drawing.Size(61, 62);
+            this.dashboardPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dashboardPicBox.TabIndex = 21;
+            this.dashboardPicBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Salmon;
+            this.button1.Location = new System.Drawing.Point(654, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 43);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Users
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 843);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userPicBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitLabel);
+            this.Controls.Add(this.exitPicBox);
+            this.Controls.Add(this.dashboardLabel);
+            this.Controls.Add(this.dashboardPicBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "Users";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Users";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardPicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private DateTimePicker joinDatedateTimePicker1;
+        private DateTimePicker userDOBdateTimePicker1;
+        private Label userPhoneLabel;
+        private Label genderLabel;
+        private ComboBox genderCb;
+        private Label joinDateLabel;
+        private TextBox userPhoneTb;
+        private Label userDOBLabel;
+        private TextBox passwordTb;
+        private Label passwordLabel;
+        private TextBox userNameTb;
+        private Label userNameLabel;
+        private Label usersLabel;
+        private Label label1;
+        private PictureBox userPicBox;
+        private PictureBox pictureBox1;
+        private Label exitLabel;
+        private PictureBox exitPicBox;
+        private Label dashboardLabel;
+        private PictureBox dashboardPicBox;
+        private Button button1;
+    }
+}
